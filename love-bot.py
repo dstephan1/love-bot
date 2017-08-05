29bff82f189b19a5668874ddc8c0d48c538e43fe
David Stephan <stephandavid3@gmail.com>
2017-08-04 22:25:45 -0700

    cleanup

--- love-bot.py
+++ love-bot.py
@@ -45,28 +45,3 @@ def done_playing():
     bot.loop.create_task(voiceclient.disconnect())
 
 bot.run(sys.argv[1])
-
-# client = discord.Client()
-
-# @client.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(client.user.name)
-#     print(client.user.id)
-#     print('------')
-
-# @client.event
-# async def on_message(message):
-#     if message.content.startswith('!test'):
-#         counter = 0
-#         tmp = await client.send_message(message.channel, 'Calculating messages...')
-#         async for log in client.logs_from(message.channel, limit=100):
-#             if log.author == message.author:
-#                 counter += 1
-
-#         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-#     elif message.content.startswith('!sleep'):
-#         await asyncio.sleep(5)
-#         await client.send_message(message.channel, 'Done sleeping')
-
-# client.run('token')
